d4ee37dace0d309b71d4f8656dc882354d577814
dupre <xavier.dupre@ensae.fr>
2015-05-24 22:56:06 +0200

    update setup to fix an issue when pyquickhelper is not here

--- setup.py
+++ setup.py
@@ -65,6 +65,10 @@ def is_local():
        "test_local_pypi" in sys.argv or \
        "upload_docs" in sys.argv or \
        "write_version" in sys.argv:
+        try:
+            import_pyquickhelper()
+        except ImportError:
+            return False
         return True
     else:
         return False
@@ -146,6 +150,7 @@ else:
     r = False
 
 if len(sys.argv) == 1 and "--help" in sys.argv:
+    pyquickhelper = import_pyquickhelper()
     pyquickhelper.process_standard_options_for_setup_help()
 
 if not r:

6cc08b402c37ddba49820e37cbbf0eb5789ec440
dupre <xavier.dupre@ensae.fr>
2015-06-07 12:20:43 +0200

    update documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -2,7 +2,8 @@ import sys
 import os
 import datetime
 import re
-import sphinxjp.themes.basicstrap
+#import sphinxjp.themes.basicstrap
+import sphinx_bootstrap_theme
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 sys.path.insert(
@@ -23,12 +24,42 @@ set_sphinx_variables(__file__,
                      "actuariat_python",
                      "Xavier Dupré, Vincent Bernardi",
                      2015,
-                     "basicstrap",
+                     "bootstrap",
                      None,
                      locals(),
                      add_extensions=None)
 
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_logo = "project_ico_small.png"
 language = "fr"
+
+html_sidebars = {}
+
+if html_theme == "bootstrap":
+    html_theme_options = {
+        'navbar_title': "home",
+        'navbar_site_name': "Site",
+        'navbar_links': [
+            ("XD", "http://www.xavierdupre.fr", True),
+            ("ENSAE",
+             "http://www.xavierdupre.fr/app/ensae_teaching_cs/helpsphinx3/index.html", True),
+            ("module", "modindex"),
+            ("index", "genindex"),
+        ],
+        'navbar_sidebarrel': False,
+        'navbar_pagenav': True,
+        'navbar_pagenav_name': "Page",
+        'globaltoc_depth': 3,
+        'globaltoc_includehidden': "true",
+        'navbar_class': "navbar navbar-inverse",
+        'navbar_fixed_top': "true",
+        'source_link_position': "nav",
+        'bootswatch_theme': "lumen",
+        # united = weird colors, sandstone=green, simplex=red, paper=trop bleu
+        # lumen: OK
+        # to try, yeti, flatly, paper
+        'bootstrap_version': "3",
+    }
 
 blog_root = "http://www.xavierdupre.fr/app/actuariat_python/helpsphinx/"
 blog_background = False

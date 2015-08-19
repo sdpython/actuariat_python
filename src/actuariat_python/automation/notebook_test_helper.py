80d04fa31a25bdccd0175516e8b390ab1ac04da6
dupre <xavier.dupre@ensae.fr>
2015-08-19 20:03:02 +0200

    specify a specific kernel before unittesting the notebooks (offline run)

--- src/actuariat_python/automation/notebook_test_helper.py
+++ src/actuariat_python/automation/notebook_test_helper.py
@@ -5,7 +5,7 @@
 """
 import os
 from pyquickhelper import noLOG
-from pyquickhelper.ipythonhelper.notebook_helper import run_notebook
+from pyquickhelper.ipythonhelper.notebook_helper import run_notebook, install_python_kernel_for_unittest
 
 
 def ls_notebooks(subfolder):
@@ -133,6 +133,7 @@ def execute_notebooks(folder, notebooks, filter,
             return False
         return True
 
+    kernel_name = install_python_kernel_for_unittest("actuariat_python")
     addpath = get_additional_paths()
     results = {}
     for i, note in enumerate(notebooks):
@@ -144,7 +145,8 @@ def execute_notebooks(folder, notebooks, filter,
                                          additional_path=addpath,
                                          valid=valid_cell,
                                          clean_function=clean_function,
-                                         fLOG=deepfLOG
+                                         fLOG=deepfLOG,
+                                         kernel_name=kernel_name
                                          )
                 if not os.path.exists(outfile):
                     raise FileNotFoundError(outfile)

07659979fcbd74c1239c0736a137828266e089c1
dupre <xavier.dupre@ensae.fr>
2016-03-28 13:33:34 +0200

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages
 #########
 
 project_var_name = "actuariat_python"
-sversion = "0.2"
+sversion = "0.3"
 versionPython = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
 path = "Lib/site-packages/" + project_var_name
 readme = 'README.rst'

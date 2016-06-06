9dc05e0c95638295a82aa20c93bb21f64fe1e178
dupre <xavier.dupre@ensae.fr>
2016-06-07 00:01:56 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -24,7 +24,7 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 set_sphinx_variables(__file__, "actuariat_python", "Xavier Dupré, Vincent Bernardi",
                      2016, "bootstrap", None,
                      locals(), add_extensions=None,
-                     extlinks=dict(issue=('https://github.com/sdpython/actuariat_python/issues/%d', 'issue')))
+                     extlinks=dict(issue=('https://github.com/sdpython/actuariat_python/issues/%s', 'issue')))
 
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 html_logo = "project_ico_small.png"

5fe3027debca59c8682baa0300219d96557f0ce3
xavier dupré <xavier.dupre@gmail.com>
2017-01-17 20:01:56 +0100

    données examen

--- _unittests/ut_exams/test_exam2016.py
+++ _unittests/ut_exams/test_exam2016.py
@@ -40,27 +40,74 @@ except ImportError:
 
 
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import add_missing_development_version
+from pyquickhelper.pycode import get_temp_folder
 
 
 class TestExam2016(unittest.TestCase):
 
     def setUp(self):
-        add_missing_development_version(
-            ["pyensae", "pymyinstall", "pyrsslocal"], __file__)
+        # add_missing_development_version(
+        #    ["pyensae", "pymyinstall", "pyrsslocal"], __file__)
+        pass
 
-    def test_person(self):
+    def test_data_2016(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        from src.actuariat_python.exams.ex2016 import enumerate_person
-        f = list(enumerate_person(1))
+        from src.actuariat_python.exams.ex2016 import enumerate_person, enumerate_appointments
+        f = list(enumerate_person(n=1))
         assert isinstance(f[0], dict)
-        df = pandas.DataFrame(enumerate_person(100))
-        self.assertEqual(df.shape, (100, 4))
+        df = pandas.DataFrame(enumerate_person(n=1000))
+        self.assertEqual(df.shape, (1000, 4))
         fLOG(df.head())
+
+        persons = df.to_dict("records")
+        ap = pandas.DataFrame(enumerate_appointments(persons))
+        fLOG(ap.head())
+
+        temp = get_temp_folder(__file__, "temp_data_2016")
+        f1 = os.path.join(temp, "persons.txt")
+        df.to_csv(f1, index=False, sep="\t")
+        f2 = os.path.join(temp, "rendezvous.txt")
+        ap.to_csv(f2, index=False, sep="\t")
+
+        assert os.path.exists(f1)
+        assert os.path.exists(f2)
+
+        """
+        Questions
+
+        1. Deux fichiers sont extraits de la base de données d'un médecin.
+           Un fichier contient des informations sur des personnes, un autre
+           sur les rendez-vous pris par ces personnes. Quels sont-ils ?
+
+        2. On souhaite étudier la relation entre le prix moyen payés par une personne,
+           son âge et son genre. Calculer le prix moyen payé par une personne ?
+
+        3. Faire la jointure entre les deux tables.
+
+        4. Tracer deux nuages de points (age, prix moyen) et (genre, prix moyen) ?
+
+        5. Calculer les coefficients de la régression prix moyen ~ age + genre.
+
+        6. On souhaite étudier le prix d'une consultation en fonction du jour de la semaine.
+           Ajouter une colonne dans la table de votre choix avec le jour de la semaine.
+
+        7. Créer un graphe moustache qui permet de vérifier cette hypothèse.
+
+        8. Ajouter une colonne dans la table de votre choix qui contient
+           365 si c'est le premier randez-vous, le nombre de jour écoulés
+           depuis le précédent rendez-vous. On appelle cette colonne delay.
+           On ajoute également la colonne 1/delay.
+
+        9. Calculer les coefficients de la régression
+           prix ~ age + genre + delay + 1/delay + jour_semaine.
+
+        10. Comment comparer ce modèle avec le précédent ?
+            Implémentez le calcul qui vous permet de répondre à cette question.
+        """
 
 
 if __name__ == "__main__":
--- src/actuariat_python/exams/ex2016.py
+++ src/actuariat_python/exams/ex2016.py
@@ -4,6 +4,7 @@
 """
 import random
 import uuid
+from datetime import datetime, timedelta
 
 
 first_names = {"M": "Balthazar", "W": "Cendrillon",
@@ -17,7 +18,7 @@ def enumerate_person(hf=0.5, age=(18, 60), n=100):
     @param      hf      proportion of men
     @param      age     range for age
     @param      n       number of person to generate
-    @return             enumerator of dictionary
+    @return             enumerator of dictionaries
     """
     for i in range(n):
         hfi = random.random() <= hf
@@ -26,7 +27,54 @@ def enumerate_person(hf=0.5, age=(18, 60), n=100):
         yield dict(gender=(1 if hfi else 0), age=agei, name=namei, idc=uuid.uuid4())
 
 
-def enumerate_appointments(age, gender, nb=2, price=60.):
+def enumerate_appointments(persons, nb=(1, 4), price=(60., 120.),
+                           first_date=None, end_date=None,
+                           formula=None):
     """
-    enuemrate a list of appointments for a given age
+    enumerate a list of appointments for a given list of ids
+
+    @param      persons     list of persons
+    @param      nb          range for the number of appointments
+    @param      price       range for prices
+    @param      begin_date  first date (or now - 2 months)
+    @param      end_date    end date (or now)
+    @param      formula     formula for the prices of the appointments
+    @return                 enumerator of dictionaries
     """
+    if end_date is None:
+        end_date = datetime.now()
+    if first_date is None:
+        first_date = end_date - timedelta(days=60)
+    if formula is None:
+        def formula_default(age, gender, when, date):
+            p = age * 0.5
+            p += -2 if gender else 2
+            if when is not None:
+                if when < 0:
+                    raise Exception(
+                        "when must be positive, when={0} - date={1}".format(when, date))
+                p -= 10 / (when + 1)
+            if date.weekday() in (6, 7):
+                p += 20
+            p += random.gauss(0, 2)
+            p += price[0]
+            p = int(p / 5) * 5
+            return p
+        formula = formula_default
+
+    for person in persons:
+        age = person["age"]
+        gender = person["gender"]
+        idc = person["idc"]
+        ns = random.randint(*nb)
+        fd = first_date
+        prev = None
+        for n in range(ns):
+            nbdays = (end_date - fd).days
+            d = random.randint(0, int(max(nbdays * 1.0 / (ns - n), 1)))
+            fd = fd + timedelta(days=d)
+            last = None if prev is None else (fd - prev).days
+            p = formula(age, gender, last, fd)
+            p = min(price[1], max(p, price[0]))
+            yield dict(idr=uuid.uuid4(), price=p, date=fd, idc=idc)
+            prev = fd

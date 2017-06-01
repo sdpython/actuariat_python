54f40111c76b109c173ed0421fb8e0b966f7c56d
xavier dupré <xavier.dupre@gmail.com>
2017-06-02 01:17:11 +0200

    move one notebook to anothe series of unit test

--- _unittests/ut_documentation/test_LONG_notebook_sessions.py
+++ _unittests/ut_documentation/test_LONG_notebook_sessions.py
@@ -43,13 +43,13 @@ from pyquickhelper.pycode import get_temp_folder, add_missing_development_versio
 from pyquickhelper.pycode import fix_tkinter_issues_virtualenv
 
 
-class TestNotebookPopulation(unittest.TestCase):
+class TestLONGNotebookSession(unittest.TestCase):
 
     def setUp(self):
         add_missing_development_version(
             ["pyensae", "pymyinstall", "pyrsslocal", "mlstatpy", "jyquickhelper"], __file__)
 
-    def test_notebook_population(self):
+    def test_LONG_notebook_session(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -58,15 +58,12 @@ class TestNotebookPopulation(unittest.TestCase):
 
         from src.actuariat_python.automation.notebook_test_helper import ls_notebooks, execute_notebooks, clean_function_notebook, unittest_raise_exception_notebook
         temp = get_temp_folder(__file__, "temp_sessions")
-        keepnote = [_ for _ in ls_notebooks(
-            "sessions") if "seance5_approche_fonctionnelle_enonce" not in _ and
-            "seance6_graphes_ml_enonce" not in _ and "election_carte_electorale_correction" not in _ and
-            "seance6_graphes_correction" not in _ and "seance6_graphes_enonce" not in _ and "ways" not in _]
+        keepnote = [_ for _ in ls_notebooks("sessions") if "ways" in _]
         self.assertTrue(len(keepnote) > 0)
         for k in keepnote:
             fLOG(k)
         fold = os.path.dirname(keepnote[0])
-        files = [os.path.join(fold, "pop-totale-france.txt")]
+        files = []
         for name in files:
             shutil.copy(name, temp)
         res = execute_notebooks(temp, keepnote,

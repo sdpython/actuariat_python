29a151e71a3a116e87f568bacbcb4eb29bcaf005
xavier dupré <xavier.dupre@gmail.com>
2017-12-16 23:32:43 +0100

    extend notebook coverage

---
+++ _unittests/ut_documentation/test_1_2_3_coverage_notebook2.py
@@ -0,0 +1,107 @@
+#-*- coding: utf-8 -*-
+"""
+@brief      test log(time=76s)
+"""
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src.actuariat_python as thismodule
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src.actuariat_python as thismodule
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.filehelper import synchronize_folder
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
+from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut, get_additional_paths
+
+
+class TestNotebook123Coverage2(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "jyquickhelper", "pyrsslocal"],
+                                        __file__, hide=True)
+
+    def a_test_notebook_runner(self, name, folder, valid=None, copy_folder=None):
+        temp = get_temp_folder(__file__, "temp_notebook_123_{0}".format(name))
+        doc = os.path.join(temp, "..", "..", "..", "_doc", "notebooks", folder)
+        self.assertTrue(os.path.exists(doc))
+        keepnote = [os.path.join(doc, _) for _ in os.listdir(doc) if name in _]
+        self.assertTrue(len(keepnote) > 0)
+
+        if copy_folder is not None:
+            if not os.path.exists(copy_folder):
+                raise FileNotFoundError(copy_folder)
+            dest = os.path.split(copy_folder)[-1]
+            dest = os.path.join(temp, dest)
+            if not os.path.exists(dest):
+                os.mkdir(dest)
+            synchronize_folder(copy_folder, dest, fLOG=fLOG)
+
+        def new_valid(cell):
+            if "print(next(it))\nprint(next(it))" in cell:
+                return False
+            return True
+
+        if valid is None:
+            valid = new_valid
+
+        import pyquickhelper
+        import jyquickhelper
+        import pymyinstall
+        import pyensae
+        import pyrsslocal
+        add_path = get_additional_paths(
+            [jyquickhelper, pyquickhelper, pyensae, pymyinstall, pyrsslocal, thismodule])
+        res = execute_notebook_list(
+            temp, keepnote, additional_path=add_path, valid=valid)
+        execute_notebook_list_finalize_ut(res, fLOG=fLOG, dump=thismodule)
+
+    def test_notebook_seance5_approche_fonctionnelle_enonce(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner(
+            "seance5_approche_fonctionnelle_enonce", "sessions")
+
+    def test_notebook_seance4_projection_population_enonce(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner(
+            "seance4_projection_population_enonce", "sessions")
+
+
+if __name__ == "__main__":
+    unittest.main()

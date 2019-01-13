c318bcba5257d65d485009814e401f87f958b5b6
xavier dupré <xavier.dupre@gmail.com>
2019-01-13 17:04:21 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -7,7 +7,7 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
 set_sphinx_variables(__file__, "Python pour un Actuaire", "Xavier Dupré",
-                     2018, "bootstrap", sphinx_bootstrap_theme.get_html_theme_path(),
+                     2019, "bootstrap", sphinx_bootstrap_theme.get_html_theme_path(),
                      locals(), add_extensions=None, book=True, nblayout='table',
                      extlinks=dict(issue=('https://github.com/sdpython/actuariat_python/issues/%s', 'issue')))
 

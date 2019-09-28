a1960bccdad782f85baae6d2f2b0d2762b38ef31
xavier dupré <xavier.dupre@gmail.com>
2019-09-29 00:51:19 +0200

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -15,7 +15,7 @@ class TestCodeStyle(ExtTestCase):
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0703', 'W0223', 'W0107'),
+                                  'C0111', 'W0703', 'W0223', 'W0107', 'C0415'),
                    skip=["Redefining built-in 'filter'",
                          "geocoding.py:174: E0702",
                          "elections.py:298: W0603",

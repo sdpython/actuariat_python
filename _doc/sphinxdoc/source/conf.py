c1fbfeffed47692d3580551fed6780bd80ebb06b
xavier dupré <xavier.dupre@gmail.com>
2021-05-27 02:25:42 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 import actuariat_python
 import pydata_sphinx_theme
 
@@ -20,8 +20,5 @@ html_split_index = True
 
 blog_root = "http://www.xavierdupre.fr/app/actuariat_python/helpsphinx/"
 blog_background = False
-html_context = {
-    'css_files': get_default_stylesheet(['_static/my-styles.css']),
-}
-
+html_css_files = ['my-styles.css']
 epkg_dictionary['machine learning'] = 'https://fr.wikipedia.org/wiki/Apprentissage_automatique'

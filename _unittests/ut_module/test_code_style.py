7294db4ccf196a5252995740c9fecbc434a3405f
xavier dupré <xavier.dupre@gmail.com>
2021-10-03 00:08:29 +0200

    lint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -15,7 +15,7 @@ class TestCodeStyle(ExtTestCase):
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0703', 'W0223', 'W0107', 'C0415'),
+                                  'C0111', 'W0703', 'W0223', 'W0107', 'C0415', 'C0209'),
                    skip=["Redefining built-in 'filter'",
                          "geocoding.py:174: E0702",
                          "elections.py:298: W0603",
@@ -26,7 +26,7 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0107', 'C0415'),
+                                  'C0111', 'W0107', 'C0415', 'C0209'),
                    skip=["Unable to import 'keyring'",
                          ])
 
